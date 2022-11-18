21e743213630326ba89854fef95cf2ad851912dd
Rogerio Mantoanelli <rogeriofotovibe@gmail.com>
2022-11-18 11:22:32 -0300

    parte 4

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-//Definindo elementos base
+//Definindo elementos base e variáveis globais
 const title = document.querySelector('title');
 const body = document.querySelector('body');
+const weekDays = ['Dom','Seg', 'Ter','Qua', 'Qui', 'Sex', 'Sab'];
 var table;
+
 
 //funções
 function setTitle(txt) {
@@ -13,72 +15,49 @@ function initTable() {
     table.setAttribute('border',1);
 }
 
-function renderHeader(){
-    const thead = document.createElement('thead');
+function renderWeekDays(){
+    const tr = document.createElement('tr');
+    for (const day of weekDays){
+        const td = document.createElement('td');
+        td.innerText = day;
+        tr.appendChild(td)
+    }
+    return tr;
+}
+
+function renderMonthName(){
     const tr = document.createElement('tr');
     const td = document.createElement('td');
     td.setAttribute('colspan',7);
     tr.appendChild(td);
-    td.innerText = "O mês vem aqui";
-    
-    const tr2 = document.createElement('tr');
-    
-    const td2 = document.createElement('td');
-    td2.innerText = 'Dom';
-    const td3 = document.createElement('td');
-    td3.innerText = 'Seg';
-    const td4 = document.createElement('td');
-    td4.innerText = 'Ter';
-    const td5 = document.createElement('td');
-    td5.innerText = 'Qua';
-    const td6 = document.createElement('td');
-    td6.innerText = 'Qui';
-    const td7 = document.createElement('td');
-    td7.innerText = 'Sex';
-    const td8 = document.createElement('td');
-    td8.innerText = 'Sab';
-    
-    tr2.appendChild(td2);
-    tr2.appendChild(td3);
-    tr2.appendChild(td4);
-    tr2.appendChild(td5);
-    tr2.appendChild(td6);
-    tr2.appendChild(td7);
-    tr2.appendChild(td8);
-    
-    thead.appendChild(tr);
-    thead.appendChild(tr2);
+    td.innerText = "Janeiro";
+    return tr;
+}
+
+function renderHeader(){
+    const thead = document.createElement('thead');
+    const trMonth = renderMonthName();
+    const trWeekDays = renderWeekDays();
+    thead.appendChild(trMonth);
+    thead.appendChild(trWeekDays);
     table.appendChild(thead);
 }
 
 function renderBody(){
     const tbody = document.createElement('tbody');
 
-    const tr = document.createElement('tr');
+    let tr = document.createElement('tr');
+    for(let x=1; x <= 31; x++){
+        const td = document.createElement('td');
+        td.innerText = x;
+        tr.appendChild(td);
+        if (x % 7 === 0){
+            tbody.appendChild(tr);
+            tr = document.createElement('tr');
+        }
+    }
     
-    const td2 = document.createElement('td');
-    td2.innerText = '1';
-    const td3 = document.createElement('td');
-    td3.innerText = '2';
-    const td4 = document.createElement('td');
-    td4.innerText = '3';
-    const td5 = document.createElement('td');
-    td5.innerText = '4';
-    const td6 = document.createElement('td');
-    td6.innerText = '5';
-    const td7 = document.createElement('td');
-    td7.innerText = '6';
-    const td8 = document.createElement('td');
-    td8.innerText = '7';
-    
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    tr.appendChild(td4);
-    tr.appendChild(td5);
-    tr.appendChild(td6);
-    tr.appendChild(td7);
-    tr.appendChild(td8);
-    
+
     tbody.appendChild(tr);
     table.appendChild(tbody);
 }
