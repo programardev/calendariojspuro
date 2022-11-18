e8c411207f8ae08bf6b6a62a7f1f3d8404fca5ee
Rogerio Mantoanelli <rogeriofotovibe@gmail.com>
2022-11-18 08:39:06 -0300

    parte 2

--- script.js
+++ script.js
@@ -2,14 +2,31 @@
 const title = document.querySelector('title');
 const body = document.querySelector('body');
 
-
 //funções
 function setTitle(txt) {
     title.innerText = txt;
 }
 
+function renderCalendar(){
+    const table = document.createElement('table');
+    table.setAttribute('border',1);
+    const tr = document.createElement('tr');
+    const td = document.createElement('td');
+    tr.appendChild(td);
+    table.appendChild(tr);
+    td.innerText = "O mês vem aqui";
+
+    const tr2 = document.createElement('tr');
+    const td2 = document.createElement('td');
+    tr2.appendChild(td2);
+    td2.innerText = 'Aqui começa os dias...';
+    table.appendChild(tr2);
+    body.appendChild(table);
+}
+
 function init(){
     setTitle("Calendário em JS PURO");
+    renderCalendar();
 }
 
 //Inicia tudo
